6edaf852795eb5c795141f01d7920874c77690fd
bheang30 <solmorobhea20@gmail.com>
2024-10-09 13:09:52 +0800

    First Commit

---
+++ ageCheck.js
@@ -0,0 +1,12 @@
+Create new file name (ageCheck.js)
+
+function ageCheck() {
+    let age = prompt("Enter your Age.");
+        if (age <=13) {
+            alert("You are a child.");
+        } else if (age <=17) {
+            alert("You are a teenager.");
+        } else {
+            alert("You are an adult.");
+        }
+}
